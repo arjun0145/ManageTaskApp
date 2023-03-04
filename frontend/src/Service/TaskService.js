df2c6d6b9947d7daade13860e10f6d5e1d243f64
alok <alokchaturvedi0145@gmail.com>
2023-03-04 08:52:47 +0530

    added url management

--- frontend/src/Service/TaskService.js
+++ frontend/src/Service/TaskService.js
@@ -1,25 +1,32 @@
 import axios from 'axios'
+import { url } from './UrlManageMent';
+
+
+
+
+// here is functions for calling api of task crud 
+
 export function save(data) {
-    return axios.post('http://localhost:7800/save/task', data)
+    return axios.post(`${url.BASE_URL}/save/task`, data)
 }
 export function GetTask(id) {
-    return axios.get('http://localhost:7800/fetch/task/'+id);
+    return axios.get(url.BASE_URL+'/fetch/task/'+id);
 }
 export function DeleteTask(id) {
     console.log(id);
-    return axios.delete('http://localhost:7800/task/delete/' + id);
+    return axios.delete(url.BASE_URL+'/task/delete/' + id);
 }
 export function incompleated() {
-    return axios.get('http://localhost:7800/incompleated');
+    return axios.get(url.BASE_URL+'/incompleated');
 }
 
 
 export function TotalCount() {
-    return axios.get('http://localhost:7800/count/task/all');
+    return axios.get(url.BASE_URL+'/count/task/all');
 }
 
 export function MarkAscompleated(id) {
-    return axios.put('http://localhost:7800/task/mark-complete/' + id);
+    return axios.put(url.BASE_URL+'/task/mark-complete/' + id);
 }
 
 
@@ -29,18 +36,18 @@ export function MarkAscompleated(id) {
 
 
 
-// here is service for user 
+// here we are calling apis  for user regisration and authentication  
 
 
 
 export function Registeruser(data) {
-    return axios.post('http://localhost:7800/savedata', data)
+    return axios.post(url.BASE_URL+'/savedata', data)
 
 }
 
 
 export function Login(data) {
-    return axios.post('http://localhost:7800/fetchdata', data)
+    return axios.post(url.BASE_URL+'/fetchdata', data)
 }
 
 
